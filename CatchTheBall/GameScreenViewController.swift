e857a28c9468093c36a960b1947e5e5ab1a1838c
Abhas Kumar <abhasroy@gmail.com>
2020-07-17 12:10:44 -0700

    added recogniser and set score

--- CatchTheBall/GameScreenViewController.swift
+++ CatchTheBall/GameScreenViewController.swift
@@ -24,12 +24,51 @@ class GameScreenViewController: UIViewController {
     @IBOutlet weak var ballTwelve: UIImageView!
     @IBOutlet weak var scoreBoard: UILabel!
     
+    var ballImages = [UIImageView]()
+    var score = 0
+    var timer = Timer()
+    var hideTimer = Timer()
+    var heighestScore = 0
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ballImages = [ballOne, ballTwo, ballThree, ballFour, ballFive, ballSix, ballSeven, ballEight, ballNine, ballTen, ballEleven, ballTwelve]
+        
+        scoreBoard.text = "Score: \(score)"
+        
+        let recogniserOne = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserTwo = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserThree = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserFour = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserFive = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserSix = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserSeven = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserEight = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserNine = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserTen = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserEleven = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recogniserTwelve = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        
+        ballOne.addGestureRecognizer(recogniserOne)
+        ballTwo.addGestureRecognizer(recogniserTwo)
+        ballThree.addGestureRecognizer(recogniserThree)
+        ballFour.addGestureRecognizer(recogniserFour)
+        ballFive.addGestureRecognizer(recogniserFive)
+        ballSix.addGestureRecognizer(recogniserSix)
+        ballSeven.addGestureRecognizer(recogniserSeven)
+        ballEight.addGestureRecognizer(recogniserEight)
+        ballNine.addGestureRecognizer(recogniserNine)
+        ballTen.addGestureRecognizer(recogniserTen)
+        ballEleven.addGestureRecognizer(recogniserEleven)
+        ballTwelve.addGestureRecognizer(recogniserTwelve)
     }
 
 
+    @objc func increaseScore() {
+        score = score + 1
+        scoreBoard.text = "Score: \(score)"
+    }
 }
