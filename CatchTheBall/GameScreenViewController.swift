a2c1871def26b44846f51d53c3526659d675852a
Abhas Kumar <abhasroy@gmail.com>
2020-07-16 22:39:05 -0700

    defined Outlets

--- CatchTheBall/GameScreenViewController.swift
+++ CatchTheBall/GameScreenViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class GameScreenViewController: UIViewController {
 
+    @IBOutlet weak var ballOne: UIImageView!
+    @IBOutlet weak var ballTwo: UIImageView!
+    @IBOutlet weak var ballThree: UIImageView!
+    @IBOutlet weak var ballFour: UIImageView!
+    @IBOutlet weak var ballFive: UIImageView!
+    @IBOutlet weak var ballSix: UIImageView!
+    @IBOutlet weak var ballSeven: UIImageView!
+    @IBOutlet weak var ballEight: UIImageView!
+    @IBOutlet weak var ballNine: UIImageView!
+    @IBOutlet weak var ballTen: UIImageView!
+    @IBOutlet weak var ballEleven: UIImageView!
+    @IBOutlet weak var ballTwelve: UIImageView!
+    @IBOutlet weak var scoreBoard: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
